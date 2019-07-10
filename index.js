6ad8947aef3ddf2e673ec1e32c23143bc8e06696
dr0id007 <nullvoid.nv@gmail.com>
2019-07-10 12:04:26 +0530

    add routes

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ app.use(bodyParser.json());
 
 // home route
 
-app.get("/", (req, res) => {
-  console.log("server request");
-  return res.send("hello");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
 });
+// app.get("/", (req, res) => {
+//   console.log("server request");
+//   return res.send("hello");
+// });
 
 // paypal
 
